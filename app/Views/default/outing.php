d65d72ee655b140caceaaeca1be9bafbf8e256ac
TobyMechant <thibaut.roux89@gmail.com>
2017-05-18 10:21:47 +0200

    Create outing.php

---
+++ app/Views/default/outing.php
@@ -0,0 +1,6 @@
+<?php $this->layout('layout', ['title' => 'Outing']) ?>
+
+<?php $this->start('main_content') ?>
+	<h2>Prochaines sorties prévues:</h2>
+	<p></p>
+<?php $this->stop('main_content') ?>
